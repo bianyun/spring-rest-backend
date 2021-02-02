38dcc7cf0f3c4246275e1e0e5e76f9725f634d11
bianyun <bianyun1981@gmail.com>
2021-02-02 22:03:15 +0800

    cors.allowed-origin 配置改名，并允许配置多个 origin

--- sr-application/src/main/java/com/silentcloud/springrest/web/config/CorsConfig.java
+++ sr-application/src/main/java/com/silentcloud/springrest/web/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.silentcloud.springrest.web.config;
 
+import cn.hutool.core.lang.Assert;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -8,16 +9,22 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
-    @Value("${cors.allowed-origin}")
-    private String allowedOrigin;
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
 
     @Bean
     public FilterRegistrationBean<CorsFilter> corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin(allowedOrigin);
+
+        Assert.notBlank(allowedOrigins, "cors.allowed-origins 不能为空");
+        String[] allowedOriginsArr = allowedOrigins.split("\\s*,\\s*");
+        config.setAllowedOrigins(Arrays.asList(allowedOriginsArr));
+
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setMaxAge(3600L);
